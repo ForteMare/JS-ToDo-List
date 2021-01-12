2400535f95bed455e25fce8b12481bdc83c1c8b9
ForteMare <30961251+ForteMare@users.noreply.github.com>
2021-01-12 21:09:07 +0100

    The most basic works

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const task = document.querySelector("#task"); // Input textfield
+// Input textfield
 const enter = document.querySelector("#enter"); // Button
 const out = document.querySelector(".out");
+
+enter.addEventListener("click", () => {
+  const task = document.querySelector("#task").value;
+  const cont = document.createElement("div");
+  cont.innerHTML = task;
+  out.appendChild(cont);
+});
