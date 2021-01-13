2a989608d7c1299d72a3c23b1da9fe36091e496b
ForteMare <30961251+ForteMare@users.noreply.github.com>
2021-01-13 22:59:46 +0100

    Cleanup

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// Input textfield
+// IO Controls
+let returnValueOf = () => document.querySelector("#task").value;
 const enter = document.querySelector("#enter"); // Button
 const out = document.querySelector(".out");
 
 enter.addEventListener("click", () => {
-  const task = document.querySelector("#task").value;
+  // Get value of input
+  const task = returnValueOf();
 
-  const cont = document.createElement("div");
-  cont.setAttribute("class", "task");
-  cont.innerHTML = task;
-  out.appendChild(cont);
+  // Create div container
+  const content = document.createElement("div");
+
+  // Set div style
+  content.setAttribute("class", "task");
+
+  // Add text content
+  content.innerHTML = task;
+
+  // Add div to parent element
+  out.appendChild(content);
 });
