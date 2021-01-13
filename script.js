6ace3165f8db7859acf58c7d6e01fcda5042b7cc
ForteMare <30961251+ForteMare@users.noreply.github.com>
2021-01-14 00:40:22 +0100

    Removed enter button, cleaned

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // IO Get
-let returnValueOf = () => document.querySelector("#task").value;
-const enter = document.querySelector("#enter"); // Button
+const inputField = document.querySelector("#task");
 const out = document.querySelector(".out");
 
-enter.addEventListener("click", () => {
-  // Get value of input
-  const task = returnValueOf();
+inputField.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    // Get value of input
+    // const task = returnValueOf();
+    const task = inputField.value;
 
-  // Create div container
-  const content = document.createElement("div");
-  const remove = document.createElement("button");
+    // Create div container
+    const content = document.createElement("div");
+    const remove = document.createElement("button");
 
-  // Set div style
-  content.setAttribute("class", "task");
-  remove.innerHTML = "Remove";
+    // Set div style
+    content.setAttribute("class", "task");
+    remove.innerHTML = "Remove";
 
-  // Add text content
-  content.innerHTML = task;
-  content.appendChild(remove);
+    // Add text content
+    content.innerHTML = task;
+    content.appendChild(remove);
 
-  // Add div to parent element
-  out.appendChild(content);
+    // Add div to parent element
+    out.appendChild(content);
 
-  remove.addEventListener("click", () => {
-    content.remove();
-  });
+    remove.addEventListener("click", () => {
+      content.remove();
+    });
+  }
 });
