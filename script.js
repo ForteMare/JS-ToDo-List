49257ad5f3399626e4aff81f76a20abebf865ba4
ForteMare <30961251+ForteMare@users.noreply.github.com>
2021-01-17 15:21:27 +0100

    Inserted icon, finalized design

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ const outputContainer = document.querySelector(".output-container");
 // Pressing enter on input field
 inputField.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
+    // Get input field value
     const todoInput = inputField.value;
 
-    // Create div for a to-do
-    const todoDiv = document.createElement("div");
-    todoDiv.innerHTML = todoInput;
+    // Create a div for text in the to-do
+    const todoLeft = document.createElement("div");
+    todoLeft.innerHTML = todoInput;
+
+    // Create an icon
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-trash-alt");
 
     // Create a button for a to-do
     const removeButton = document.createElement("button");
-    removeButton.innerHTML = "Remove";
+
+    // Create div for a to-do
+    const todoDiv = document.createElement("div");
 
     // Set div style
     todoDiv.setAttribute("class", "todo-style");
 
     // Append
+    removeButton.appendChild(icon);
+    todoDiv.appendChild(todoLeft);
     todoDiv.appendChild(removeButton);
     outputContainer.appendChild(todoDiv);
 
